5ccd4af3025620771afb0e54123345833954042a
Mutwassim Haider <mutwassim@gmail.com>
2025-05-15 03:29:22 +0500

    added profile picture api

--- backend/src/controllers/auth.controllers.js
+++ backend/src/controllers/auth.controllers.js
@@ -1,6 +1,7 @@
 import { generateToken } from '../lib/utils.js';
 import User from '../models/user.model.js';
 import bcrypt from 'bcryptjs';
+import cloudinary from '../lib/cloudinary.js'
 
 export const signup = async (req,res)=>{         
 
@@ -85,5 +86,33 @@ export const logout = (req,res)=>{
     } catch (error) {
         console.log("Error in logout controller", error.message);
         res.status(500).json({message: "Internal Server Error"})
+    }
+}
+
+export const updateProfile = async (req,res)=>{
+    try {
+        const {profilePic} = req.body;
+        const userId = req.user._id;
+
+        if(!profilePic){
+            res.status(400).json({message:"Profile pic is required"})
+        }
+
+        const uploadResponse = await cloudinary.uploader.upload(profilePic)
+        const updatedUser = await User.findByIdAndUpdate(userId,{profilePic:uploadResponse.secure_url},{new:true})
+
+        res.status(200).json({message:updatedUser})
+    } catch (error) {
+        console.log("Error in updating profile ",error.message)
+        res.status(500).json({message:"Internal Server Error"})
+    }
+}
+
+export const checkAuth = (req,res)=>{
+    try {
+        res.status(200).json(req.user)
+    } catch (error) {
+        console.log("Error in check auth controller ",error.message)
+        res.status(500).json({message:"Internal Server Error"})
     }
 }
