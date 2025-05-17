5787651da240d854b47947c37add3189f9528271
Mutwassim Haider <mutwassim@gmail.com>
2025-05-17 16:12:36 +0500

    updated cloudinary

--- backend/src/controllers/message.controllers.js
+++ backend/src/controllers/message.controllers.js
@@ -1,6 +1,6 @@
 import User from "../models/user.model.js"
 import Message from "../models/messages.model.js"
-
+import cloudinary from "../lib/cloudinary.js";
 
 export const getUsersForSidebar = async (req,res)=>{
     try {
